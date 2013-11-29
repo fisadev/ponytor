fc75e7fdef5dcdec670a88028f9c1334137d6591
Juan Pedro Fisanotti <fisadev@gmail.com>
2013-11-29 18:32:08 -0300

    Up version for minor release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='ponytor',
-    version='0.1.1',
+    version='0.1.2',
     description=u'A small command line utility and python lib to run automated things when files change',
     long_description=open('README.rst').read(),
     author = u'Juan Pedro Fisanotti',
